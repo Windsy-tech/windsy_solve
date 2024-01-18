3f80247b8582791dd373fbe4980248ae16b24ca2
Manjunath Muntha Purushotham <windsy.gmbh@gmail.com>
2024-01-18 16:01:42 +0100

    Add new files and update sign-in functionality

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -25,8 +25,8 @@ class SignInButton extends ConsumerWidget {
     return ElevatedButton(
       onPressed: () => signInWithEmailAndPassword(
         ref,
-        'ma.mu@windsy.de',
-        '123456',
+        'felix.wernicke@windsy.de',
+        'windsy',
         context,
       ),
       style: ElevatedButton.styleFrom(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:windsy_solve/core/common/widgets/home_navigation_button.dart';
 import 'package:windsy_solve/core/common/widgets/labelled_text.dart';
 
 void main() {
@@ -21,6 +22,31 @@ void main() {
       // Verify that our counter starts at 0.
       expect(find.text('label'), findsOneWidget);
       expect(find.text('text'), findsOneWidget);
+    });
+  });
+
+  group('HomeNavigationButton', () {
+    testWidgets('HomeNavigationButton', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+              body: Column(
+            children: [
+              HomeNavigationButton(
+                title: 'label',
+                icon: Icons.add,
+                onTap: () {},
+                count: 1,
+              ),
+            ],
+          )),
+        ),
+      );
+
+      // Verify that our counter starts at 0.
+      expect(find.text('label'), findsOneWidget);
+      expect(find.byIcon(Icons.add), findsOneWidget);
     });
   });
 }
