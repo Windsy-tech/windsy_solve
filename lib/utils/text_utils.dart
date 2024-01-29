b72fc64e0daa2be4bcdc0e83bce4297aeaf04cbd
Manjunath Muntha Purushotham <mpm.nath007@gmail.com>
2024-01-29 09:33:50 +0100

    Add shortenFileName method to TextUtils

--- lib/utils/text_utils.dart
+++ lib/utils/text_utils.dart
@@ -6,4 +6,15 @@ extension TextUtils on String {
         .map((str) => str[0].toUpperCase() + str.substring(1))
         .join(' ');
   }
+
+  String shortenFileName(int maxLength) {
+    if (length <= maxLength) {
+      return this;
+    }
+
+    String extension = split('.').last;
+    String truncatedName = substring(0, maxLength - extension.length - 1);
+
+    return '$truncatedName...$extension';
+  }
 }
