41cca2bb93e3c3dc6068e2e9e5e334abe7592575
mpmnath <mpm.nath007@gmail.com>
2024-02-09 16:29:18 +0100

    Update error text style in ErrorText widget

--- lib/core/common/error_text.dart
+++ lib/core/common/error_text.dart
@@ -7,7 +7,12 @@ class ErrorText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(error),
+      child: Text(
+        error,
+        style: Theme.of(context).textTheme.labelMedium!.copyWith(
+              color: Theme.of(context).colorScheme.error,
+            ),
+      ),
     );
   }
 }
