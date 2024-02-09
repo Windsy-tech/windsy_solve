5b5cb4dbbfb72b63c7cf432f07a0c01e9f746121
mpmnath <mpm.nath007@gmail.com>
2024-02-09 16:29:28 +0100

    Add theme customization to search delegates

--- lib/features/nc/delegates/nc_assign_search_delegate.dart
+++ lib/features/nc/delegates/nc_assign_search_delegate.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:windsy_solve/core/common/error_text.dart';
 import 'package:windsy_solve/core/common/loader.dart';
 import 'package:windsy_solve/features/nc/controller/nc_controller.dart';
+import 'package:windsy_solve/theme/color_palette.dart';
 import 'package:windsy_solve/utils/text_utils.dart';
 
 class NCAssignSearchDelegate extends SearchDelegate {
@@ -51,39 +52,59 @@ class NCAssignSearchDelegate extends SearchDelegate {
   }
 
   @override
+  ThemeData appBarTheme(BuildContext context) {
+    final theme = Theme.of(context);
+    return Theme.of(context).copyWith(
+      scaffoldBackgroundColor: theme.colorScheme.onBackground,
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: theme.textTheme.labelMedium,
+        border: InputBorder.none,
+      ),
+    );
+  }
+
+  @override
   Widget buildSuggestions(BuildContext context) {
+    final theme = Theme.of(context);
     return ref
         .watch(searchMembersProvider(TextUtils.capitalizeFirstLetter(query)))
         .when(
-          data: (users) => ListView.builder(
-            itemCount: users.length,
-            itemBuilder: (BuildContext context, int index) {
-              final user = users[index];
-              return CheckboxListTile(
-                value: assignedTo.contains(user.uid),
-                /* title: CircleAvatar(
-                  backgroundImage: NetworkImage(user.photoUrl),
-                ), */
-                title: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 18,
-                      backgroundImage: NetworkImage(user.photoUrl),
-                    ),
-                    const SizedBox(width: 10),
-                    Text(user.displayName),
-                  ],
-                ),
-                onChanged: (value) {
-                  if (assignedTo.contains(user.uid)) {
-                    assignedTo.remove(user.uid);
-                    onAssign(assignedTo);
-                  } else {
-                    addAssignedTo(user.uid);
-                  }
-                },
-              );
-            },
+          data: (users) => Container(
+            decoration: BoxDecoration(
+              gradient: theme.brightness == Brightness.dark
+                  ? ColorPalette.darkSurface
+                  : ColorPalette.lightSurface,
+            ),
+            child: ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (BuildContext context, int index) {
+                final user = users[index];
+                return CheckboxListTile(
+                  value: assignedTo.contains(user.uid),
+                  title: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundImage: NetworkImage(user.photoUrl),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        user.displayName,
+                        style: theme.textTheme.labelLarge,
+                      ),
+                    ],
+                  ),
+                  onChanged: (value) {
+                    if (assignedTo.contains(user.uid)) {
+                      assignedTo.remove(user.uid);
+                      onAssign(assignedTo);
+                    } else {
+                      addAssignedTo(user.uid);
+                    }
+                  },
+                );
+              },
+            ),
           ),
           error: (error, stackTrace) => ErrorText(
             error: error.toString(),
