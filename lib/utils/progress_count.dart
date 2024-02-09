6015a85e27aabeeae05303791b4e28768eaf9e9d
mpmnath <mpm.nath007@gmail.com>
2024-02-09 16:29:47 +0100

    Add download progress indicator function

---
+++ lib/utils/progress_count.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+String downloadProgressIndicator(ImageChunkEvent? loadingProgress) {
+  if (loadingProgress == null) return "";
+  return loadingProgress.expectedTotalBytes != null
+      ? "${((loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!) * 100).toInt().toString()} %"
+      : "";
+}
