3f23088603da562068945bc1ef11e8a38e03c7d6
Windsy GmbH <windsy.gmbh@gmail.com>
2024-02-12 09:52:44 +0100

    Add themedata

--- lib/features/inspection/delegates/inspection_windfarm_search_delegate.dart
+++ lib/features/inspection/delegates/inspection_windfarm_search_delegate.dart
@@ -5,6 +5,7 @@ import 'package:windsy_solve/core/common/error_text.dart';
 import 'package:windsy_solve/core/common/loader.dart';
 import 'package:windsy_solve/features/nc/controller/nc_controller.dart';
 import 'package:windsy_solve/models/windfarm_model.dart';
+import 'package:windsy_solve/theme/color_palette.dart';
 import 'package:windsy_solve/utils/text_utils.dart';
 
 class InspectionWindFarmSearchDelegate extends SearchDelegate {
@@ -46,27 +47,50 @@ class InspectionWindFarmSearchDelegate extends SearchDelegate {
   }
 
   @override
+  ThemeData appBarTheme(BuildContext context) {
+    final theme = Theme.of(context);
+    return Theme.of(context).copyWith(
+      // hide textfield underline
+
+      scaffoldBackgroundColor: theme.colorScheme.onBackground,
+      inputDecorationTheme: InputDecorationTheme(
+        hintStyle: theme.textTheme.labelLarge,
+        border: InputBorder.none,
+      ),
+    );
+  }
+
+
+  @override
   Widget buildSuggestions(BuildContext context) {
+    final theme = Theme.of(context);
     return ref.watch(getWindFarmsProvider(query)).when(
-          data: (windFarms) => ListView.builder(
-            itemCount: windFarms.length,
-            itemBuilder: (BuildContext context, int index) {
-              final data = windFarms[index];
-              return ListTile(
-                onTap: () => selectWindFarm(context, data),
-                title: Text(
-                  //Capitalise the first letter of each word
-                  TextUtils.capitalizeFirstLetter(data.windFarm!),
-                ),
-                subtitle: Text(
-                  'Turbine No: ${data.turbineNo}',
-                ),
-                trailing: data.windFarm == windFarm.windFarm &&
-                        data.turbineNo == windFarm.turbineNo
-                    ? const Icon(Icons.check)
-                    : const SizedBox(),
-              );
-            },
+          data: (windFarms) => Container(
+            decoration: BoxDecoration(
+              gradient: theme.brightness == Brightness.dark
+                  ? ColorPalette.darkSurface
+                  : ColorPalette.lightSurface,
+            ),
+            child: ListView.builder(
+              itemCount: windFarms.length,
+              itemBuilder: (BuildContext context, int index) {
+                final data = windFarms[index];
+                return ListTile(
+                  onTap: () => selectWindFarm(context, data),
+                  title: Text(
+                    //Capitalise the first letter of each word
+                    TextUtils.capitalizeFirstLetter(data.windFarm!),
+                  ),
+                  subtitle: Text(
+                    'Turbine No: ${data.turbineNo}',
+                  ),
+                  trailing: data.windFarm == windFarm.windFarm &&
+                          data.turbineNo == windFarm.turbineNo
+                      ? const Icon(Icons.check)
+                      : const SizedBox(),
+                );
+              },
+            ),
           ),
           error: (error, stackTrace) => ErrorText(
             error: error.toString(),
