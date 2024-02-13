bb86ecc9c8a4ed6aa4722fc3c3a4ba05a3b37a0e
Windsy-tech <windsy.gmbh@gmail.com>
2024-02-13 07:27:26 +0100

    Refactor general setting screen layout and theme handling

--- lib/features/settings/screens/general_setting_screen.dart
+++ lib/features/settings/screens/general_setting_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
+import 'package:windsy_solve/core/common/widgets/label_widget.dart';
 import 'package:windsy_solve/core/constants/constants.dart';
-import 'package:windsy_solve/theme/pallete.dart';
+import 'package:windsy_solve/theme/color_palette.dart';
 import 'package:windsy_solve/theme/provider/theme_provider.dart';
 
 class GeneralSettings extends ConsumerWidget {
@@ -19,30 +20,42 @@ class GeneralSettings extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeNotifierProvider.notifier).mode;
+    final theme = Theme.of(context);
     return Scaffold(
+      backgroundColor: theme.colorScheme.background,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text('Profile Screen'),
         leading: IconButton(
           onPressed: () => navigateToSettingsPage(context),
           icon: const Icon(Icons.arrow_back),
         ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                title: const Text("Theme"),
-                trailing: Switch.adaptive(
-                  value: themeMode == ThemeMode.dark ? true : false,
-                  onChanged: (val) {
-                    toggleTheme(ref);
-                  },
-                ),
-              )
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: theme.brightness == Brightness.dark
+              ? ColorPalette.darkSurface
+              : ColorPalette.lightSurface,
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: const LabelWidget("Theme"),
+                  trailing: Switch.adaptive(
+                    value: themeMode == ThemeMode.dark ? true : false,
+                    onChanged: (val) {
+                      toggleTheme(ref);
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
