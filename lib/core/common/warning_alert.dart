459ae69824d5f1091ebb1d2e6189d83b8fb84cb6
Windsy-tech <windsy.gmbh@gmail.com>
2024-02-13 07:27:05 +0100

    Add warning alert dialog

---
+++ lib/core/common/warning_alert.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:windsy_solve/core/common/alert_dialog.dart';
+
+Future showWarningExitAlert(BuildContext context) async {
+  await showAlertDialog(
+    context: context,
+    title: 'Warning',
+    content: 'Are you sure you want to exit?',
+    defaultActionText: 'Yes',
+  );
+  if (context.mounted) Navigator.pop(context);
+}
+
+Future showWarningSaveAlertDialog(BuildContext context) {
+  final theme = Theme.of(context);
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        icon: const Icon(Icons.warning, color: Colors.yellow),
+        title: Text(
+          "Warning!",
+          style: theme.textTheme.headlineLarge,
+        ),
+        content: Text(
+          "Inorder to create a section, you need to save the inspection first",
+          style: theme.textTheme.bodyMedium,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              //saveInspection();
+            },
+            child: Text(
+              "OK",
+              style: theme.textTheme.labelMedium,
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
