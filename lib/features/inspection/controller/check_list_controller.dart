2c2fd3441d226d967428e97a3757ebdb803771f7
mpmnath <mpm.nath007@gmail.com>
2024-02-13 16:57:27 +0100

    Add imports and update methods in CheckListController

--- lib/features/inspection/controller/check_list_controller.dart
+++ lib/features/inspection/controller/check_list_controller.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 import 'package:windsy_solve/core/providers/storage_repository_provider.dart';
-import 'package:windsy_solve/features/inspection/controller/inspection_controller.dart';
+import 'package:windsy_solve/features/auth/controller/auth_controller.dart';
 import 'package:windsy_solve/features/inspection/repository/check_list_repository.dart';
 import 'package:windsy_solve/models/checklist_model.dart';
+import 'package:windsy_solve/utils/snack_bar.dart';
 
 final checkListControllerProvider =
     StateNotifierProvider<CheckListController, bool>((ref) {
@@ -19,7 +22,18 @@ final getCheckListByIdProvider =
     StreamProvider.family((ref, CheckListModel checkListModel) {
   final checkListController = ref.watch(checkListControllerProvider.notifier);
   return checkListController.getCheckListById(
-      checkListModel.section, checkListModel.id);
+    checkListModel.inspectionId,
+    checkListModel.section,
+    checkListModel.id,
+  );
+});
+
+final getComponentsProvider = StreamProvider.family((ref, String query) {
+  return ref.watch(checkListControllerProvider.notifier).getComponents(query);
+});
+
+final getChecksProvider = StreamProvider.family((ref, String query) {
+  return ref.watch(checkListControllerProvider.notifier).getChecks(query);
 });
 
 class CheckListController extends StateNotifier<bool> {
@@ -36,8 +50,78 @@ class CheckListController extends StateNotifier<bool> {
         _storageRepository = storageRepository,
         super(false);
 
-  Stream<CheckListModel> getCheckListById(String section, String checkListId) {
+  //update checkList
+  void updateCheckList(
+    BuildContext context,
+    String checkListId,
+    CheckListModel checkList,
+  ) async {
+    final user = _ref.watch(userProvider)!;
+    final res = await _checkListRepository.updateCheckList(
+      user.companyId,
+      checkListId,
+      checkList,
+    );
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, r.toString());
+        //Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  //Adds a new custom component to firebase
+  void addNewComponent(BuildContext context, String component) async {
+    final user = _ref.watch(userProvider)!;
+    final res = await _checkListRepository.addNewComponent(
+      user.companyId,
+      component,
+    );
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, r.toString());
+        //Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  //Adds a new custom check to firebase
+  void addNewCheck(BuildContext context, String check) async {
+    final user = _ref.watch(userProvider)!;
+    final res = await _checkListRepository.addNewCheck(
+      user.companyId,
+      check,
+    );
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, r.toString());
+        //Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  //get check list by id
+  Stream<CheckListModel> getCheckListById(
+      String inspectionId, String section, String checkListId) {
     //final inspection = _ref.watch(inspectionControllerProvider.notifier);
-    return _checkListRepository.getCheckListById(section, checkListId);
+    return _checkListRepository.getCheckListById(
+        inspectionId, section, checkListId);
+  }
+
+  //get components
+  Stream<List<String>> getComponents(String query) {
+    final user = _ref.read(userProvider);
+    final companyName = user!.companyName;
+    return _checkListRepository.getComponents(companyName, query);
+  }
+
+  //get checks
+  Stream<List<String>> getChecks(String query) {
+    final user = _ref.read(userProvider);
+    final companyName = user!.companyName;
+    return _checkListRepository.getChecks(companyName, query);
   }
 }
--- lib/features/inspection/repository/check_list_repository.dart
+++ lib/features/inspection/repository/check_list_repository.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:windsy_solve/core/failure.dart';
 import 'package:windsy_solve/core/providers/firebase_providers.dart';
+import 'package:windsy_solve/core/type_defs.dart';
 import 'package:windsy_solve/models/checklist_model.dart';
 
 final checkListRepositoryProvider = Provider<CheckListRepository>((ref) {
@@ -15,14 +18,131 @@ class CheckListRepository {
 
   CollectionReference get _companies => _firestore.collection('companies');
 
-  Stream<CheckListModel> getCheckListById(String section, String checkListId) {
+  //add new component
+  FutureEither<String> addNewComponent(
+    String companyName,
+    String component,
+  ) async {
+    try {
+      await _companies
+          .doc(companyName.toLowerCase())
+          .collection('checkListData')
+          .doc('component')
+          .update({
+        'values': FieldValue.arrayUnion([component])
+      });
+      return right('New Component added');
+    } on FirebaseException catch (e) {
+      return left(Failure(message: e.message!));
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
+  }
+
+  //add new check
+  FutureEither<String> addNewCheck(
+    String companyName,
+    String check,
+  ) async {
+    try {
+      await _companies
+          .doc(companyName.toLowerCase())
+          .collection('checkListData')
+          .doc('check')
+          .update({
+        'values': FieldValue.arrayUnion([check])
+      });
+      return right('New Check added');
+    } on FirebaseException catch (e) {
+      return left(Failure(message: e.message!));
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
+  }
+
+  //update checkList
+  FutureEither<String> updateCheckList(
+    String companyName,
+    String checkListId,
+    CheckListModel checkList,
+  ) async {
+    try {
+      await _companies
+          .doc(companyName.toLowerCase())
+          .collection('inspections')
+          .doc(checkList.inspectionId)
+          .collection(checkList.section)
+          .doc(checkListId)
+          .update(checkList.toMap());
+      return right('Updated successfully');
+    } on FirebaseException catch (e) {
+      return left(Failure(message: e.message!));
+    } catch (e) {
+      return left(Failure(message: e.toString()));
+    }
+  }
+
+  //get check list by id
+  Stream<CheckListModel> getCheckListById(
+    String inspectionId,
+    String section,
+    String checkListId,
+  ) {
     return _companies
         .doc('windsy')
         .collection('inspections')
-        .doc(section)
-        .collection('checklist')
+        .doc(inspectionId)
+        .collection(section)
         .doc(checkListId)
         .snapshots()
         .map((snapshot) => CheckListModel.fromMap(snapshot.data()!));
+  }
+
+  //get components
+  Stream<List<String>> getComponents(String companyName, String query) {
+    if (query.length <= 3) {
+      // Return an empty stream if the query is too short
+      return Stream.value([]);
+    }
+    return _companies
+        .doc(companyName.toLowerCase())
+        .collection('checkListData')
+        .doc('component')
+        .snapshots()
+        .map((event) {
+      List<String> values = event['values'].cast<String>();
+      print(values); // Use values instead of event.data()!['values']
+
+      // Filter values based on the query
+      List<String> filteredValues = values
+          .where((value) => value.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+
+      return filteredValues;
+    });
+  }
+
+  //get checks
+  Stream<List<String>> getChecks(String companyName, String query) {
+    if (query.length <= 3) {
+      // Return an empty stream if the query is too short
+      return Stream.value([]);
+    }
+    return _companies
+        .doc(companyName.toLowerCase())
+        .collection('checkListData')
+        .doc('check')
+        .snapshots()
+        .map((event) {
+      List<String> values = event['values'].cast<String>();
+      print(values); // Use values instead of event.data()!['values']
+
+      // Filter values based on the query
+      List<String> filteredValues = values
+          .where((value) => value.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+
+      return filteredValues;
+    });
   }
 }
