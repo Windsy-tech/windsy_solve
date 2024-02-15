8d5e35717db8db7d526a35f5b63ff3ce9f0df0f0
mpmnath <mpm.nath007@gmail.com>
2024-02-15 17:34:14 +0100

    Add localDataProvider to firebase_providers.dart

--- lib/core/providers/firebase_providers.dart
+++ lib/core/providers/firebase_providers.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:windsy_solve/core/providers/sync_task/sync_task.dart';
 
 final firestoreProvider = Provider((ref) => FirebaseFirestore.instance);
 final authProvider = Provider((ref) => FirebaseAuth.instance);
 final storageProvider = Provider((ref) => FirebaseStorage.instance);
+final localDataProvider = Provider((ref) => LocalDatabase());
