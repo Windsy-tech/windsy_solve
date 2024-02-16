b31e15990b76c2a00a9e2c07866cd788ff8d0c12
Windsy-tech <windsy.gmbh@gmail.com>
2024-02-16 12:01:30 +0100

    Refactor sync task count and add tooltip

--- lib/features/home/screens/pending_sync/pending_sync_badge.dart
+++ lib/features/home/screens/pending_sync/pending_sync_badge.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:windsy_solve/core/providers/firebase_providers.dart';
+import 'package:windsy_solve/core/providers/sync_task/sync_task_controller.dart';
 
 class PendingSyncBadge extends ConsumerWidget {
   const PendingSyncBadge({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final syncTasks = ref.watch(localDataProvider).getNCSyncTasksCount();
+    final syncTasks = ref.watch(localDatabaseProvider).getTotalSyncTasksCount();
     return FutureBuilder(
       future: syncTasks,
       builder: (context, s) {
         return IconButton(
-          onPressed: () => Routemaster.of(context).push('/pending-sync'),
+          tooltip: s.data != 0 ? '${s.data} Sync Pending' : 'No Sync Pending',
+          disabledColor: const Color.fromRGBO(158, 158, 158, 1),
+          onPressed: () => s.data != 0
+              ? Routemaster.of(context).push('/pending-sync')
+              : null,
           icon: Badge(
             label: Text(s.data.toString()),
             isLabelVisible: s.data != 0,
-            child: const Icon(
+            child: Icon(
               Icons.sync_rounded,
               size: 24,
+              color: s.data != 0 ? Colors.red : Colors.grey,
             ),
           ),
         );
