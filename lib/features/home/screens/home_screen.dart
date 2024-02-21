616008d4c1e8f19db1050a577fec0302aa9826c2
Windsy-tech <windsy.gmbh@gmail.com>
2024-02-21 08:08:28 +0100

    Add HomeAnalytics widget to HomeScreen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:windsy_solve/core/common/widgets/connectivity_status.dart';
 import 'package:windsy_solve/features/home/drawer/drawer_widget.dart';
+import 'package:windsy_solve/features/home/screens/home_analytics.dart';
 import 'package:windsy_solve/features/home/screens/home_navigations.dart';
 import 'package:windsy_solve/features/home/screens/pending_sync/pending_sync_badge.dart';
 import 'package:windsy_solve/theme/color_palette.dart';
@@ -11,6 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
 
     return Scaffold(
       drawer: const DrawerWidget(),
@@ -30,18 +32,23 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Container(
+        height: size.height,
         decoration: BoxDecoration(
           gradient: theme.brightness == Brightness.dark
               ? ColorPalette.darkSurface
               : ColorPalette.lightSurface,
         ),
         child: const SafeArea(
-            child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              HomeNavigations(),
-            ],
+            child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                HomeNavigations(),
+                SizedBox(height: 16.0),
+                HomeAnalytics(),
+              ],
+            ),
           ),
         )),
       ),
