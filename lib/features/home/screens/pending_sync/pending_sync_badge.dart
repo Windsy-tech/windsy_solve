2b74dfe8c2ecc064ee7a685d4ee2c1679287144d
Windsy-tech <windsy.gmbh@gmail.com>
2024-02-21 08:08:23 +0100

    Refactor PendingSyncBadge widget to use Riverpod and add error handling

--- lib/features/home/screens/pending_sync/pending_sync_badge.dart
+++ lib/features/home/screens/pending_sync/pending_sync_badge.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
+import 'package:windsy_solve/core/common/error_text.dart';
+import 'package:windsy_solve/core/common/loader.dart';
 import 'package:windsy_solve/features/home/screens/pending_sync/controller/sync_task_controller.dart';
 
 class PendingSyncBadge extends ConsumerWidget {
@@ -8,28 +10,27 @@ class PendingSyncBadge extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final syncTasks =
-        ref.watch(syncTaskControllerProvider).getTotalSyncTasksCount();
-    return FutureBuilder(
-      future: syncTasks,
-      builder: (context, s) {
-        return IconButton(
-          tooltip: s.data != 0 ? '${s.data} Sync Pending' : 'No Sync Pending',
-          disabledColor: const Color.fromRGBO(158, 158, 158, 1),
-          onPressed: () => s.data != 0
-              ? Routemaster.of(context).push('/pending-sync')
-              : null,
-          icon: Badge(
-            label: Text(s.data.toString()),
-            isLabelVisible: s.data != 0,
-            child: Icon(
-              Icons.sync_rounded,
-              size: 24,
-              color: s.data != 0 ? Colors.red : Colors.grey,
-            ),
-          ),
+    return ref.watch(getTotalSyncTasksCountProvider).when(
+          data: (count) {
+            return IconButton(
+              tooltip: count != 0 ? '$count Sync Pending' : 'No Sync Pending',
+              disabledColor: const Color.fromRGBO(158, 158, 158, 1),
+              onPressed: () => count != 0
+                  ? Routemaster.of(context).push('/pending-sync')
+                  : null,
+              icon: Badge(
+                label: Text(count.toString()),
+                isLabelVisible: count != 0,
+                child: Icon(
+                  Icons.sync_rounded,
+                  size: 24,
+                  color: count != 0 ? Colors.red : Colors.grey,
+                ),
+              ),
+            );
+          },
+          error: (e, s) => ErrorText(error: e.toString()),
+          loading: () => const Loader(),
         );
-      },
-    );
   }
 }
