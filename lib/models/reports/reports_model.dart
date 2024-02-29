9ebf8bc524d9ff0ab085a9322cfe99d14c48eb16
mpmnath <mpm.nath007@gmail.com>
2024-02-29 16:24:55 +0100

    Update ReportModel with version and type properties

--- lib/models/reports/reports_model.dart
+++ lib/models/reports/reports_model.dart
@@ -1,24 +1,28 @@
-import 'dart:convert';
+//Reports Model
 
 class ReportModel {
   final String id;
   final String title;
+  final int version;
+  final String type;
   final String associatedTo;
   final String createdBy;
   final DateTime createdAt;
   final String status;
-  final String fileLink;
+  final String fileUrl;
   final String fileType;
   final String fileName;
 
   ReportModel({
     required this.id,
     required this.title,
+    required this.version,
+    required this.type,
     required this.associatedTo,
     required this.createdBy,
     required this.createdAt,
     required this.status,
-    required this.fileLink,
+    required this.fileUrl,
     required this.fileType,
     required this.fileName,
   });
@@ -26,22 +30,26 @@ class ReportModel {
   ReportModel copyWith({
     String? id,
     String? title,
+    int? version,
+    String? type,
     String? associatedTo,
     String? createdBy,
     DateTime? createdAt,
     String? status,
-    String? fileLink,
+    String? fileUrl,
     String? fileType,
     String? fileName,
   }) {
     return ReportModel(
       id: id ?? this.id,
       title: title ?? this.title,
+      version: version ?? this.version,
+      type: type ?? this.type,
       associatedTo: associatedTo ?? this.associatedTo,
       createdBy: createdBy ?? this.createdBy,
       createdAt: createdAt ?? this.createdAt,
       status: status ?? this.status,
-      fileLink: fileLink ?? this.fileLink,
+      fileUrl: fileUrl ?? this.fileUrl,
       fileType: fileType ?? this.fileType,
       fileName: fileName ?? this.fileName,
     );
@@ -51,11 +59,13 @@ class ReportModel {
     return <String, dynamic>{
       'id': id,
       'title': title,
+      'version': version,
+      'type': type,
       'associatedTo': associatedTo,
       'createdBy': createdBy,
       'createdAt': createdAt.millisecondsSinceEpoch,
       'status': status,
-      'fileLink': fileLink,
+      'fileUrl': fileUrl,
       'fileType': fileType,
       'fileName': fileName,
     };
@@ -65,11 +75,13 @@ class ReportModel {
     return ReportModel(
       id: map['id'] as String,
       title: map['title'] as String,
+      version: map['version'] as int,
+      type: map['type'] as String,
       associatedTo: map['associatedTo'] as String,
       createdBy: map['createdBy'] as String,
       createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt'] as int),
       status: map['status'] as String,
-      fileLink: map['fileLink'] as String,
+      fileUrl: map['fileUrl'] as String,
       fileType: map['fileType'] as String,
       fileName: map['fileName'] as String,
     );
@@ -77,7 +89,7 @@ class ReportModel {
 
   @override
   String toString() {
-    return 'ReportModel(id: $id, title: $title, associatedTo: $associatedTo, createdBy: $createdBy, createdAt: $createdAt, status: $status, fileLink: $fileLink, fileType: $fileType, fileName: $fileName)';
+    return 'ReportModel(id: $id, title: $title, version: $version, type: $type, associatedTo: $associatedTo, createdBy: $createdBy, createdAt: $createdAt, status: $status, fileUrl: $fileUrl, fileType: $fileType, fileName: $fileName)';
   }
 
   @override
@@ -86,11 +98,13 @@ class ReportModel {
 
     return other.id == id &&
         other.title == title &&
+        other.version == version &&
+        other.type == type &&
         other.associatedTo == associatedTo &&
         other.createdBy == createdBy &&
         other.createdAt == createdAt &&
         other.status == status &&
-        other.fileLink == fileLink &&
+        other.fileUrl == fileUrl &&
         other.fileType == fileType &&
         other.fileName == fileName;
   }
@@ -99,11 +113,13 @@ class ReportModel {
   int get hashCode {
     return id.hashCode ^
         title.hashCode ^
+        version.hashCode ^
+        type.hashCode ^
         associatedTo.hashCode ^
         createdBy.hashCode ^
         createdAt.hashCode ^
         status.hashCode ^
-        fileLink.hashCode ^
+        fileUrl.hashCode ^
         fileType.hashCode ^
         fileName.hashCode;
   }
