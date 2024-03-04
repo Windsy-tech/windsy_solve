d15c9371f7fc6f40ae22e531acf7bd8905995963
Windsy-tech <windsy.gmbh@gmail.com>
2024-03-04 14:49:49 +0100

    Refactor date picker styling and add validation for start and end dates

--- lib/features/inspection/widgets/inspection_end_date.dart
+++ lib/features/inspection/widgets/inspection_end_date.dart
@@ -3,37 +3,55 @@ import 'package:windsy_solve/core/common/widgets/label_widget.dart';
 import 'package:windsy_solve/utils/date_time_utils.dart';
 
 class InspectionEndDate extends StatelessWidget {
+  final DateTime startDate;
   final DateTime endDate;
   final ValueChanged<DateTime?>? onChanged;
   const InspectionEndDate({
     super.key,
+    required this.startDate,
     required this.endDate,
     this.onChanged,
   });
 
   Future<DateTime?> showDateTimePicker(BuildContext context) {
-    print(endDate);
+    final theme = Theme.of(context);
     return showDatePicker(
       context: context,
       initialDate: endDate,
       firstDate: DateTime(1900),
       lastDate: DateTime(2300),
+      initialEntryMode: DatePickerEntryMode.calendarOnly,
+      builder: (context, child) => Theme(
+        data: theme.copyWith(
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+            ),
+          ),
+        ),
+        child: child!,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-        final theme = Theme.of(context);
+    final theme = Theme.of(context);
 
     return ListTile(
       contentPadding: const EdgeInsets.all(0),
-      leading: const LabelWidget(
-        'End date'
-      ),
+      leading: const LabelWidget('End date'),
       trailing: TextButton(
         onPressed: () async {
           final dateTime = await showDateTimePicker(context);
-          onChanged?.call(dateTime);
+          if (dateTime == null) {
+            return;
+          } else {
+            if (dateTime.isAfter(startDate) ||
+                dateTime.isAtSameMomentAs(startDate)) {
+              onChanged?.call(endDate);
+            }
+          }
         },
         child: Text(
           endDate.toDateString(),
--- lib/features/inspection/widgets/inspection_start_date.dart
+++ lib/features/inspection/widgets/inspection_start_date.dart
@@ -4,20 +4,35 @@ import 'package:windsy_solve/utils/date_time_utils.dart';
 
 class InspectionStartDate extends StatelessWidget {
   final DateTime startDate;
+  final DateTime endDate;
   final ValueChanged<DateTime?>? onChanged;
   const InspectionStartDate({
     super.key,
     required this.startDate,
+    required this.endDate,
     this.onChanged,
   });
 
   Future<DateTime?> showDateTimePicker(BuildContext context) {
-    print(startDate);
+    final theme = Theme.of(context);
     return showDatePicker(
       context: context,
       initialDate: startDate,
       firstDate: DateTime(1900),
       lastDate: DateTime(2300),
+      initialEntryMode: DatePickerEntryMode.calendarOnly,
+      builder: (context, child) => Theme(
+        data: theme.copyWith(
+          primaryColor: theme.colorScheme.primary,
+          indicatorColor: theme.colorScheme.onPrimary,
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+            ),
+          ),
+        ),
+        child: child!,
+      ),
     );
   }
 
@@ -26,13 +41,18 @@ class InspectionStartDate extends StatelessWidget {
     final theme = Theme.of(context);
     return ListTile(
       contentPadding: const EdgeInsets.all(0),
-      leading: const LabelWidget(
-        'Start date'
-      ),
+      leading: const LabelWidget('Start date'),
       trailing: TextButton(
         onPressed: () async {
           final dateTime = await showDateTimePicker(context);
-          onChanged?.call(dateTime);
+          if (dateTime == null) {
+            return;
+          } else {
+            if (dateTime.isBefore(endDate) ||
+                dateTime.isAtSameMomentAs(endDate)) {
+              onChanged?.call(endDate);
+            }
+          }
         },
         child: Text(
           startDate.toDateString(),
